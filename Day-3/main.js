f8eafe02034f365901c4098dabbbbb2fa6a03339
Karl <karllouiserito08@gmail.com>
2021-07-22 16:01:40 +0800

    added learnings and refactor day 3

--- Day-3/main.js
+++ Day-3/main.js
@@ -1,10 +1,10 @@
-const inputs = document.querySelectorAll('header input')
+const inputs = document.querySelectorAll('input')
 
-function handleUpdate() {
+
+function handleUpdates() {
     const suffix = this.dataset.size || '';
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate))
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
+inputs.forEach(input => input.addEventListener('change', handleUpdates))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdates))
